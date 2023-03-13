83fae1840a9006038d7d6712bcc1787794632cf9
htrvu <trongvulqd@gmail.com>
2023-03-14 02:30:07 +0700

    add inference

--- README.md
+++ README.md
@@ -1,16 +1,52 @@
 # Deformable DETR Working Repo
 This repo makes slight changes to the original Deformable-DETR repo for easy training/finetuning purposes. And also addresses some errors. 
 
-## Usage
-1. First Go through [original-repo readme](https://github.com/fundamentalvision/Deformable-DETR) first for setup. 
+## Installation
 
-NOTE : 
-Do this Before the `Compiling CUDA operators` in above README.
 ```bash
-# First check nvidia-driver exists
-nvidia-smi
-# Incase you are using gcloud(debian machine) and `nvidia-smi` command is not working, run `install-driver.sh` to fresh install nvidia-driver. Not sure if it works for other linux distros.
-./install-driver.sh
+pip install -r requirements.txt
+cd ./models/ops
+sh ./make.sh
+python test.py
+cd ../../
 ```
 
-2. After you have setup the environment, checkout [how_to.md](https://github.dev/robinnarsinghranabhat/Deformable-DETR/how_to.md)
+## Training
+
+Structure of dataset:
+
+```
+    |-- custom_data/
+        |-- annotations/
+            |-- custom_train.json
+            |-- custom_val.json
+        |-- train2017
+            |-- *.jpg
+        |-- val2017
+            |-- *.jpg
+```
+
+Download the pretrained-weights from the [original repo](https://github.com/fundamentalvision/Deformable-DETR#main-results) and put it in the current folder.
+
+```bash
+python -u main.py \
+    --output_dir exps/exp0 \
+    --with_box_refine --two_stage \
+    --resume ./r50_deformable_detr_plus_iterative_bbox_refinement_plus_plus_two_stage-checkpoint.pth \
+    --coco_path ./custom_data \
+    --num_classes 8 \
+    --epochs 50 \
+    --batch_size 8 \
+    --lr 1e-4 \
+    --lr_drop 20
+```
+
+## Inference
+
+```bash
+python inference.py \
+    --resume exps/exp0/checkpoint.pth \
+    --input_dir ./inferences/test_images \
+    --output_dir ./inferences/test_images_results \
+    --num_classes 8
+```
---
+++ inference.py
@@ -0,0 +1,194 @@
+import argparse
+import json
+import random
+from PIL import Image, ImageDraw
+import torchvision.transforms as T
+
+from tqdm import tqdm
+import os
+import numpy as np
+import torch
+import util.misc as utils
+from util import box_ops
+from models import build_model
+
+#!python demo.py --resume modelzoo/r50_deformable_detr-checkpoint.pth --img_path '/content/Deformable-DETR/Brazil vs Germany Men Football Final Rio 201.png'
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('Deformable DETR Inference', add_help=False)
+
+    parser.add_argument('--lr', default=2e-4, type=float)
+    parser.add_argument('--lr_backbone_names', default=["backbone.0"], type=str, nargs='+')
+    parser.add_argument('--lr_backbone', default=2e-5, type=float)
+    parser.add_argument('--lr_linear_proj_names', default=['reference_points', 'sampling_offsets'], type=str, nargs='+')
+    parser.add_argument('--lr_linear_proj_mult', default=0.1, type=float)
+    parser.add_argument('--batch_size', default=2, type=int)
+    parser.add_argument('--weight_decay', default=1e-4, type=float)
+    parser.add_argument('--epochs', default=50, type=int)
+    parser.add_argument('--lr_drop', default=40, type=int)
+    parser.add_argument('--lr_drop_epochs', default=None, type=int, nargs='+')
+    parser.add_argument('--clip_max_norm', default=0.1, type=float,
+                        help='gradient clipping max norm')
+    parser.add_argument('--sgd', action='store_true')
+
+    # Variants of Deformable DETR
+    parser.add_argument('--with_box_refine', default=False, action='store_true')
+    parser.add_argument('--two_stage', default=False, action='store_true')
+
+    # Model parameters
+    parser.add_argument('--frozen_weights', type=str, default=None,
+                        help="Path to the pretrained model. If set, only the mask head will be trained")
+
+    # * Backbone
+    parser.add_argument('--backbone', default='resnet50', type=str,
+                        help="Name of the convolutional backbone to use")
+    parser.add_argument('--dilation', action='store_true',
+                        help="If true, we replace stride with dilation in the last convolutional block (DC5)")
+    parser.add_argument('--position_embedding', default='sine', type=str, choices=('sine', 'learned'),
+                        help="Type of positional embedding to use on top of the image features")
+    parser.add_argument('--position_embedding_scale', default=2 * np.pi, type=float,
+                        help="position / size * scale")
+    parser.add_argument('--num_feature_levels', default=4, type=int, help='number of feature levels')
+
+    # * Transformer
+    parser.add_argument('--enc_layers', default=6, type=int,
+                        help="Number of encoding layers in the transformer")
+    parser.add_argument('--dec_layers', default=6, type=int,
+                        help="Number of decoding layers in the transformer")
+    parser.add_argument('--dim_feedforward', default=1024, type=int,
+                        help="Intermediate size of the feedforward layers in the transformer blocks")
+    parser.add_argument('--hidden_dim', default=256, type=int,
+                        help="Size of the embeddings (dimension of the transformer)")
+    parser.add_argument('--dropout', default=0.1, type=float,
+                        help="Dropout applied in the transformer")
+    parser.add_argument('--nheads', default=8, type=int,
+                        help="Number of attention heads inside the transformer's attentions")
+    parser.add_argument('--num_queries', default=300, type=int,
+                        help="Number of query slots")
+    parser.add_argument('--dec_n_points', default=4, type=int)
+    parser.add_argument('--enc_n_points', default=4, type=int)
+
+    # * Segmentation
+    parser.add_argument('--masks', action='store_true',
+                        help="Train segmentation head if the flag is provided")
+
+    # Loss
+    parser.add_argument('--no_aux_loss', dest='aux_loss', action='store_false',
+                        help="Disables auxiliary decoding losses (loss at each layer)")
+
+    # * Matcher
+    parser.add_argument('--set_cost_class', default=2, type=float,
+                        help="Class coefficient in the matching cost")
+    parser.add_argument('--set_cost_bbox', default=5, type=float,
+                        help="L1 box coefficient in the matching cost")
+    parser.add_argument('--set_cost_giou', default=2, type=float,
+                        help="giou box coefficient in the matching cost")
+
+    # * Loss coefficients
+    parser.add_argument('--mask_loss_coef', default=1, type=float)
+    parser.add_argument('--dice_loss_coef', default=1, type=float)
+    parser.add_argument('--cls_loss_coef', default=2, type=float)
+    parser.add_argument('--bbox_loss_coef', default=5, type=float)
+    parser.add_argument('--giou_loss_coef', default=2, type=float)
+    parser.add_argument('--focal_alpha', default=0.25, type=float)
+
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=42, type=int)
+    parser.add_argument('--resume', default='', help='resume from checkpoint')
+    parser.add_argument('--num_classes', default=91, type=int) # By default, Model was trained on 91 classes
+
+    # dataset parameters
+    parser.add_argument('--dataset_file', default='coco')
+    parser.add_argument('--coco_path', default='./data/coco', type=str)
+    parser.add_argument('--coco_panoptic_path', type=str)
+    parser.add_argument('--remove_difficult', action='store_true')
+
+    # Data for inference
+    parser.add_argument('--input_dir', type=str, required=True, help='path to folder of images to run inference')
+    parser.add_argument('--output_dir', type=str, required=True, help='path to output folder')
+    parser.add_argument('--num_workers', default=1, type=int)
+    parser.add_argument('--cache_mode', default=False, action='store_true', help='whether to cache images on memory')
+
+    # Inference params
+    parser.add_argument('--conf-thres', default=0.4, type=float, help='confident threshold for keeping boxes')
+
+    return parser
+
+# standard PyTorch mean-std input image normalization
+transform = T.Compose([
+    T.Resize(800),
+    T.ToTensor(),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+def main(args):
+    utils.init_distributed_mode(args)
+    print("git:\n  {}\n".format(utils.get_sha()))
+
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    seed = args.seed + utils.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    model, criterion, postprocessors = build_model(args)
+    model.to(device)
+    checkpoint = torch.load(args.resume, map_location='cpu')
+    model.load_state_dict(checkpoint['model'], strict=False)
+    if torch.cuda.is_available():
+        model.cuda()
+    model.eval()
+
+    output_folder = args.output_dir
+    os.makedirs(output_folder, exist_ok=True)
+
+    img_names = os.listdir(args.input_dir)
+    for name in tqdm(img_names):
+        path = os.path.join(args.input_dir, name)
+
+        # mean-std normalize the input image (batch-size: 1)
+        im = Image.open(path)
+        img = transform(im).unsqueeze(0)
+        img=img.cuda()
+
+        # propagate through the model
+        outputs = model(img)
+
+        out_logits, out_bbox = outputs['pred_logits'], outputs['pred_boxes']
+        prob = out_logits.sigmoid()
+        topk_values, topk_indexes = torch.topk(prob.view(out_logits.shape[0], -1), 100, dim=1)
+        scores = topk_values
+        topk_boxes = topk_indexes // out_logits.shape[2]
+        labels = topk_indexes % out_logits.shape[2]
+        boxes = box_ops.box_cxcywh_to_xyxy(out_bbox)
+        boxes = torch.gather(boxes, 1, topk_boxes.unsqueeze(-1).repeat(1,1,4))
+
+        # filter boxes with conf < thres
+        keep = scores[0] > args.conf_thres
+        boxes = boxes[0, keep]
+        labels = labels[0, keep]
+
+        # relative [0, 1] to absolute [0, height] coordinates
+        im_h, im_w = im.size
+        target_sizes = torch.tensor([[im_w, im_h]])
+        target_sizes = target_sizes.cuda()
+        img_h, img_w = target_sizes.unbind(1)
+        scale_fct = torch.stack([img_w, img_h, img_w, img_h], dim=1)
+        boxes = boxes * scale_fct[:, None, :]
+
+        # save results
+        source_img = Image.open(path).convert("RGBA")
+        draw = ImageDraw.Draw(source_img)
+        for xmin, ymin, xmax, ymax in boxes[0].tolist():
+            draw.rectangle(((xmin, ymin), (xmax, ymax)), outline ="red")
+        source_img.save(os.path.join(output_folder, name), "png")
+
+        # results = [{'conf': s, 'class': l, 'box': b} for s, l, b in zip(scores, labels, boxes)]
+
+if __name__ == '__main__':
+    parser = get_args_parser()
+    args = parser.parse_args()
+    main(args)
